e3c5f6ff3e81023f94392228a4254e6ec54c5558
Tomas Votruba <tomas.vot@gmail.com>
2025-07-02 10:32:59 +0200

    app

--- scoper.php
+++ scoper.php
@@ -11,7 +11,7 @@ $timestamp = $nowDateTime->format('Ym');
 return [
     'prefix' => 'SmokeTestgen' . $timestamp,
     'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
-    'exclude-namespaces' => ['#^Rector\\\\SmokeTestgen#', '#^App\\', '#^Symfony\\\\Polyfill#', '#^PHPUnit\\\\'],
+    'exclude-namespaces' => ['#^Rector\\\\SmokeTestgen#', '#^App\\\\#', '#^App#', '#^Symfony\\\\Polyfill#', '#^PHPUnit\\\\'],
     'exclude-classes' => ['AppKernel', 'Kernel', 'App\\Kernel'],
     'exclude-files' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
