c3b4bf917d65000d56c10cf98139258f5a66c8a5
Tomas Votruba <tomas.vot@gmail.com>
2025-07-02 10:15:53 +0200

    fixup! cs

--- src/Templating/TemplateDecorator.php
+++ src/Templating/TemplateDecorator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Rector\SmokeTestgen\Templating;
 
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+
 final class TemplateDecorator
 {
     public function decorate(string $templateContents, string $smokeTestsDirectory): string
@@ -36,6 +39,18 @@ final class TemplateDecorator
 
     private function adjustNamespace(string $templateContents, string $smokeTestsDirectory): string
     {
+        $composerJsonFilePath = getcwd() . '/composer.json';
+        if (file_exists($composerJsonFilePath)) {
+            $projectComposerJson = Json::decode(FileSystem::read($composerJsonFilePath));
+
+            $requireDevPsr4 = $projectComposerJson['require-dev']['psr-4'] ?? null;
+
+            dump($requireDevPsr4);
+            die;
+        }
+
+        $projectComposerJson = json_decode(file_get_contents($composerJsonFilePath), true);
+
         if ($smokeTestsDirectory === 'tests/Unit/Smoke') {
             // default one, nothing to adjust
             return $templateContents;
