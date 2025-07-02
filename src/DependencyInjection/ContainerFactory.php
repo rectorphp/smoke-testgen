461c6da8d6e6d25a0c2c5895233f5969bd381462
GitHub Action <action@github.com>
2025-07-02 07:30:53 +0000

    release PHP 7.2 downgraded

--- src/DependencyInjection/ContainerFactory.php
+++ src/DependencyInjection/ContainerFactory.php
@@ -1,76 +1,56 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\SmokeTestgen\DependencyInjection;
 
-use Illuminate\Container\Container;
+use SmokeTestgen202507\Illuminate\Container\Container;
 use Rector\SmokeTestgen\Console\SmokeTestgenConsoleApplication;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Finder\Finder;
-use Webmozart\Assert\Assert;
-
+use SmokeTestgen202507\Symfony\Component\Console\Application;
+use SmokeTestgen202507\Symfony\Component\Finder\Finder;
+use SmokeTestgen202507\Webmozart\Assert\Assert;
 final class ContainerFactory
 {
     /**
      * @api used in bin and tests
      */
-    public function create(): Container
+    public function create() : Container
     {
         $container = new Container();
-
         // console
-        $container->singleton(Application::class, function (Container $container): Application {
+        $container->singleton(Application::class, function (Container $container) : Application {
             $smokeTestgenConsoleApplication = new SmokeTestgenConsoleApplication('Rector Smoke Testgen');
             $smokeTestgenConsoleApplication->setDefaultCommand('generate');
-
             $commandClasses = $this->findCommandClasses();
-
             // register commands
             foreach ($commandClasses as $commandClass) {
                 $command = $container->make($commandClass);
                 $smokeTestgenConsoleApplication->add($command);
             }
-
             // remove basic command to make output clear
             $this->hideDefaultCommands($smokeTestgenConsoleApplication);
-
             return $smokeTestgenConsoleApplication;
         });
-
         return $container;
     }
-
-    public function hideDefaultCommands(Application $application): void
+    public function hideDefaultCommands(Application $application) : void
     {
-        $application->get('list')
-            ->setHidden(true);
-        $application->get('completion')
-            ->setHidden(true);
-        $application->get('help')
-            ->setHidden(true);
+        $application->get('list')->setHidden(\true);
+        $application->get('completion')->setHidden(\true);
+        $application->get('help')->setHidden(\true);
     }
-
     /**
      * @return string[]
      */
-    private function findCommandClasses(): array
+    private function findCommandClasses() : array
     {
-        $commandFinder = Finder::create()
-            ->files()
-            ->name('*Command.php')
-            ->in(__DIR__ . '/../Command');
-
+        $commandFinder = Finder::create()->files()->name('*Command.php')->in(__DIR__ . '/../Command');
         $commandClasses = [];
         foreach ($commandFinder as $commandFile) {
             $commandClass = 'Rector\\SmokeTestgen\\Command\\' . $commandFile->getBasename('.php');
-
             // make sure it exists
             Assert::classExists($commandClass);
-
             $commandClasses[] = $commandClass;
         }
-
         return $commandClasses;
     }
 }
---
+++ vendor/illuminate/contracts/Validation/ValidatorAwareRule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SmokeTestgen202507\Illuminate\Contracts\Validation;
+
+use SmokeTestgen202507\Illuminate\Validation\Validator;
+interface ValidatorAwareRule
+{
+    /**
+     * Set the current validator.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return $this
+     */
+    public function setValidator(Validator $validator);
+}
